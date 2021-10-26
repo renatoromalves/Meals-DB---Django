22612d458d741ac01e90b601db4fa1c65c6903b1
Renato Martins <renatoroma@hotmail.com>
2021-10-26 00:25:42 -0300

    correção procfile2

--- Mealsdb/views.py
+++ Mealsdb/views.py
@@ -25,6 +25,7 @@ def index(request):
 
 def search(request):
     search_key = request.GET.get('newsearch')
+    search_key2 = request.GET.get('searchfield')
     if search_key:
         meals.search(search_key)
         base_url = reverse('search_view')
@@ -37,8 +38,14 @@ def search(request):
         except TypeError:
             return render(request, 'error.html', {'mealsearch': search_key})
     else:
-        meals.search(search_key2)
-        paginator = Paginator(meals.search_list, qtde_pag)
-        page_number = request.GET.get('page', 1)
-        page_obj = paginator.get_page(page_number)
-        return render(request, 'list.html', {'page_obj': page_obj,'mealsearch': request.GET.get('searchfield')})
+        try:
+            meals.search(search_key2)
+            paginator = Paginator(meals.search_list, qtde_pag)
+            page_number = request.GET.get('page', 1)
+            page_obj = paginator.get_page(page_number)
+            return render(request, 'list.html', {'page_obj': page_obj,'mealsearch': request.GET.get('searchfield')})
+        except TypeError:
+            paginator = Paginator(meals.search_list, qtde_pag)
+            page_number = request.GET.get('page', 1)
+            page_obj = paginator.get_page(page_number)
+            return render(request, 'list.html', {'page_obj': page_obj,'mealsearch': request.GET.get('searchfield')})
